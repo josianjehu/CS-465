b2a1e5e3ee8fd94d177dad582974a8bd12b155bf
Jehu Domenech <josianjehu@gmail.com>
2023-04-07 09:40:37 -0400

    added admin page with angular

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers/trips');
+var tripsController = require('../controllers/trips');
 
 /* GET home page. */
-router.route("/trips").get(controller.tripList);
+router.route('/trips').get(tripsController.tripList).post(tripsController.tripsAddTrip);
 
-router.route("/trips/tripCode").get(controller.tripFindCode);
+router.route("/trips/tripCode").get(tripsController.tripFindCode).put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
