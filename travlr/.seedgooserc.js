d49c075d33b5c3d80c83f2be0c66d4bcdf25c965
Jehu Domenech <josianjehu@gmail.com>
2023-04-22 04:13:48 -0400

    dded travel to module 8`

---
+++ travlr/.seedgooserc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    modelBaseDirectory:"app_api/database/models",
+    models: ["*.js", "!db.js"],
+    data: "data",
+    db: "mongodb://127.0.0.1:27017/travlr",
+    // db: "mongodb://localhost:27017/travlr",
+};
